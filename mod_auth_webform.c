5e26d74d67f3776def6c13a7468779c81fbfaf73
Jeff Mataya <jeff.mataya@gmail.com>
2011-09-29 23:27:44 -0400

    Interim commit.

--- mod_auth_webform.c
+++ mod_auth_webform.c
@@ -20,10 +20,27 @@
 #include <http_log.h>
 #include <http_protocol.h>
 
+// APR References
+#include "apr.h"
+#include "apr_strings.h"
+#include "apr_hash.h"
+#include "apr_user.h"
+#include "apr_lib.h"
+#include "apr_signal.h"
+#include "apr_global_mutex.h"
+#include "apr_dbm.h"
+
 // Linux References
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+// Other references
+#include <stdlib.h>
+
+// Constants
+#define URL_MAX_LEN 2000    // Based on research that 2,000 is what browsers
+                            // will implement at a maximum.
 
 //
 // Get a value from the Apache configuration.
@@ -43,6 +60,121 @@ static char * get_conf_value(request_rec *r, char *key) {
     return value;
 }
 
+//
+// Strips the http(s):// from the beginning of the URL.
+// Inputs:
+//  (*) url - The raw URL that is inputted from the user.
+//            TODO: Do some testing around XSS here.
+// Returns:
+//  (*) The new normalized URL.
+//
+static char * normalize_url(request_rec *r, char *url) {
+    char *pch;
+    char *return_url = NULL;
+    int i = 0;
+    
+    // Some basic error checking.
+    if (url == NULL || strlen(url) > URL_MAX_LEN) {
+        return_url = apr_palloc(r->pool, sizeof(char));
+        return_url[0] = '\0';
+        return return_url;
+    }
+    else if (url[strlen(url)-1] != '\0') {
+        // Ensure that the string is null terminated.
+        url[strlen(url)-1] = '\0';
+    }
+    
+    // Initialize the return URL.
+    return_url = apr_palloc(r->pool, (strlen(url) + 1) * sizeof(char));     
+ 
+    // Attempt to retrieve the document root from httpd.conf.
+
+    login_filename = get_conf_value(r, "auth_page");
+    if (login_filename == NULL) {
+        ap_log_rerror(
+            APLOG_MARK,
+            APLOG_ERR,
+            0,
+            r,
+            "mod_auth_webform: Env variable auth_page not found in httpd.conf");
+        return HTTP_INTERNAL_SERVER_ERROR;
+    }   
+
+    // Tokenize and reform the URL.
+    pch = strtok(url, "\\/");
+    while (pch != NULL) {
+        if (strcmp(pch, "http") != 0 && strcmp(pch, "https") != 0) {
+            // Append a slash if this isn't the first concatenation.
+            if (i++ > 0) {
+                strcat(return_url, "/");
+            }
+            
+            // Append the segment.
+            strcat(return_url, pch);
+        }
+        
+        // Get the next segement.
+        pch = strtok(NULL, "\\/");
+    }
+    
+    return return_url;
+}
+
+static char * my_cat(request_rec *r, char *destination, char *source) {
+    int new_size = 0;
+    int index_old = 0;
+    int index_new = 0;
+    char *new_string = NULL;
+    char to_copy;
+    
+    // Allocate the new string.
+    new_size = strlen(destination) + strlen(source) + 1;
+    new_string = apr_palloc(r->pool, new_size * sizeof(char));
+    
+    // Copy the old string.
+    while (index_old < strlen(destination) && destination[index_old] != '\0') {
+        to_copy = destination[index_old];
+        new_string[index_new] = to_copy;
+        
+        index_new++;
+        index_old++;
+    }
+    
+    // Copy the new string.
+    index_old = 0;
+    while (index_old < strlen(source) && source[index_new] != '\0') {
+        // Copy.
+        new_string[index_new] = source[index_old];
+        index_new++;
+        index_old++;
+    }
+    
+    new_string[index_new] = '\0';
+    new_string = escape_uri(r->pool, new_string);
+    return new_string;
+}
+
+static char * craft_login_url(request_rec *r, char *login_url, char *return_url) {
+    char *final_url;
+    char *fixed_return_url;
+    int size;
+    
+    // Fix the return URL.
+    fixed_return_url = normalize_url(r, return_url);
+    
+    // Get the size of the new URL.
+    size = strlen(login_url) + strlen(fixed_return_url) + 2;
+    
+    // Set the new URL.
+    final_url = apr_palloc(r->pool, size * sizeof(char));
+    //strcat(final_url, login_url);
+    final_url = my_cat(r, final_url, login_url);
+    strcat(final_url, "?redir=");
+    strcat(final_url, fixed_return_url);
+    
+    return final_url;
+}
+
 static int mod_auth_webform_handler(request_rec *r) {
      
     apr_file_t *fd;
@@ -51,6 +183,7 @@ static int mod_auth_webform_handler(request_rec *r) {
     char *login_filename;
     struct stat st;
     int size;
+    char *final_login_url;
 
     // Ensure that this handler is being requested.
     if (!r->handler || strcmp(r->handler, "mod_auth_webform")) {
@@ -92,7 +225,8 @@ static int mod_auth_webform_handler(request_rec *r) {
     // TODO: Obviously, we don't want to always redirect to Google. Replace this
     // with the value taken from the configuration file.
     //
-    apr_table_setn(r->headers_out, "Location", login_filename);
+    final_login_url = craft_login_url(r, login_filename, r->filename);
+    apr_table_setn(r->headers_out, "Location", final_login_url);
     return HTTP_MOVED_TEMPORARILY;
 }
 
